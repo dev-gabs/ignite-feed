9d781ac338f8015748e1bb1b7e08b443c4c032e3
Gabriel Figueiredo Salvador <gabs.fsalvador@gmail.com>
2023-01-24 22:46:50 -0300

    Posts updated dynamically through properties

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,37 @@ import { Header } from './components/Header'
 import { Post } from './components/Post'
 import { Sidebar } from './components/Sidebar'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare.'},
+      { type: 'link', content: '👉 jane.design/doctorcare'},      
+    ],
+    publishedAt: new Date ('2023-01-10 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare.'},
+      { type: 'link', content: '👉 jane.design/doctorcare'},      
+    ],
+    publishedAt: new Date ('2023-01-15 20:00:00')
+  },
+]
+
 import './global.css'
 import styles from './App.module.css'
 
@@ -16,8 +47,15 @@ export function App() {
         </aside>
 
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                author = {post.author}
+                content = {post.content}
+                publishedAt = {post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
